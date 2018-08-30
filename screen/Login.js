23ba61310626756f6789f2283492794605fde156
Matheus Pimentel <matheus200713@gmail.com>
2018-08-30 16:58:09 -0300

    salvando no asyncStorage

--- screen/Login.js
+++ screen/Login.js
@@ -1,10 +1,30 @@
 import React, {Component} from 'react';
-import {StyleSheet, Text, View, Button, ProgressBarAndroid, Image, TextInput} from 'react-native';
+import {StyleSheet, Text, View, Button, Image, TextInput} from 'react-native';
+import {AsyncStorage} from 'react-native';
 
 export default class Login extends React.Component {
   static navigationOptions = {
     title: 'Login',
   };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      senha: ''
+    };
+  }
+
+  async save (navigate) {
+    try {
+      navigate('Main', { name: 'Main' })
+      await AsyncStorage.setItem('profile', JSON.stringify(this.state))
+      console.log('Deu certo!')
+    } catch (e) {
+      console.log('error saving data' + e)
+    }
+  }
+
   render() {
     const { navigate } = this.props.navigation;
     return (
@@ -13,11 +33,27 @@ export default class Login extends React.Component {
           style={styles.circleImage}
           source={require('../images/user.png')}
         />
+        <Text/>
+
+        <TextInput
+          style={{height: 40, width: 200, marginTop: 15, borderColor: 'gray', borderWidth: 1, padding: 5}}
+          onChangeText={(email) => this.setState({email})}
+          placeholder='Digite seu email'
+          value={this.state.email}
+        />
+
+        <TextInput
+          style={{height: 40, width: 200, marginTop: 15, borderColor: 'gray', borderWidth: 1, padding: 5}}
+          onChangeText={(senha) => this.setState({senha})}
+          placeholder='Digite seu email'
+          secureTextEntry={true}
+          value={this.state.senha}
+        />
+
         <Button
           title="Login"
           onPress={() =>
-            navigate('Main', { name: 'Main' })}/>
-        <ProgressBarAndroid />
+            this.save(navigate)}/>
       </View>
     );
   }
